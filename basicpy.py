e0a6ad31e93d73d4aaf8c5cb0d5317e89c59a441
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-06-03 16:49:05 +0600

    Update basicpy.py

--- basicpy.py
+++ basicpy.py
@@ -1,3 +1,45 @@
+
+print("hello")
+a=5
+b=10
+print(a+b)
+print("hey",6,7,sep="$")
+print("hey",4,5,sep=":",end="009\n")
+print("hello \n arman\n how")
+a=1
+b=2.4
+c=False
+d=complex(8,2)
+e="arman"
+print("the type is a ",type(a))
+print("the type is a ",type(c))
+print("the type is a ",type(d))
+print("the type is a ",type(e))
+print("the type is a ",type(b))
+ 
+list=[6,3,2.4,[4,5],["apple","cat"]] #list are mutable and can be changed after creation 
+print(list)
+
+tuple=("parrot","cat","dog")#tuple are immutable cannot be changed after creation
+print(tuple)
+
+dict={"name":"arman","age":"20","canVote":True}
+print(dict)
+print(dict)
+
+
+print(15/2)
+print(15//6)
+print(5%3)
+print(2**4)
+
+
+#type casting 
+a="1"
+b="3"
+print(int(a)+int(b))
+
+
 #string /string slicing/ string method in python 
 name="arman hossain"
 print(name[0:5])
