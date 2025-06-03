a648af5c532fb4c8ef48a96191f34753c9a5ae61
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-06-03 10:06:28 +0600

    first commit

---
+++ basicpy.py
@@ -0,0 +1,26 @@
+print("hello")
+a=5
+b=10
+print(a+b)
+print("hey",6,7,sep="$")
+print("hey",4,5,sep=":",end="009\n")
+print("hello \n arman\n how")
+a=1
+b=2.4
+c=False
+d=complex(8,2)
+e="arman"
+print("the type is a ",type(a))
+print("the type is a ",type(c))
+print("the type is a ",type(d))
+print("the type is a ",type(e))
+print("the type is a ",type(b))
+ 
+list=[6,3,2.4,[4,5],["apple","cat"]] #list are mutable and can be changed after creation 
+print(list)
+
+tuple=("parrot","cat","dog")#tuple are immutable cannot be changed after creation
+print(tuple)
+
+dict={"name":"arman","age":"20","canVote":True}
+print(dict)
