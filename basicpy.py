88b8c8495db9664ca971188ed2500d6c14554a0b
Hysenburg <156664441+Hysenburg@users.noreply.github.com>
2025-07-07 20:17:36 +0600

    Update basicpy.py

--- basicpy.py
+++ basicpy.py
@@ -1,15 +1,3 @@
-a="arman"
-
-for i in a:
-    print(i)
-    if i=="a":
-        print("something special")
-         
-
-#code 2
-list=["man","womw","girl","boy"]
-for i in list:
-    print(i)
 
 
 
